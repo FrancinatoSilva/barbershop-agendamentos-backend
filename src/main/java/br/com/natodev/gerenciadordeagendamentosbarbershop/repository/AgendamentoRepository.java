ae4fdd470eb88e0090ddf59e69e3de9282b67a49
NatoDev <irenofrancinato@gmail.com>
2026-06-03 14:52:03 -0300

    refactor: adicionar anotacao necessaria nas classes repository

--- src/main/java/br/com/natodev/gerenciadordeagendamentosbarbershop/repository/AgendamentoRepository.java
+++ src/main/java/br/com/natodev/gerenciadordeagendamentosbarbershop/repository/AgendamentoRepository.java
@@ -2,9 +2,11 @@ package br.com.natodev.gerenciadordeagendamentosbarbershop.repository;
 
 import br.com.natodev.gerenciadordeagendamentosbarbershop.domain.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 
+@Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
     boolean existsByDataHora(LocalDateTime dataHora);
--- src/main/java/br/com/natodev/gerenciadordeagendamentosbarbershop/repository/ServicoRepository.java
+++ src/main/java/br/com/natodev/gerenciadordeagendamentosbarbershop/repository/ServicoRepository.java
@@ -2,6 +2,8 @@ package br.com.natodev.gerenciadordeagendamentosbarbershop.repository;
 
 import br.com.natodev.gerenciadordeagendamentosbarbershop.domain.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ServicoRepository extends JpaRepository<Servico, Long> {
 }
