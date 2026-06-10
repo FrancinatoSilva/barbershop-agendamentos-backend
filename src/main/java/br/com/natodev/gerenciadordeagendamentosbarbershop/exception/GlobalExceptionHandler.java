f604bc9dd80049bc0a1a3d3908ea2887a980a1f2
NatoDev <irenofrancinato@gmail.com>
2026-06-10 17:34:34 -0300

    feat: implementa camada de global exception handler

---
+++ src/main/java/br/com/natodev/gerenciadordeagendamentosbarbershop/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package br.com.natodev.gerenciadordeagendamentosbarbershop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleRegrasDeNegocio(IllegalArgumentException exception) {
+        Map<String, String> resposta = new HashMap<>();
+        resposta.put("erro", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resposta);
+    }
+}
